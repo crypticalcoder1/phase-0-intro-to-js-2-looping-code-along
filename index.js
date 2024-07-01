6142323ef072f4de3d0ac137b6cc2330a2129ba7
crypticalcoder1 <kirambawycliff@gmail.com>
2024-07-01 14:56:02 +0300

    my work

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solutions in this file
+// Code your solutions in this file\
+
+function writeCards(names, eventName) {
+    let thankYouCards = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      thankYouCards.push(message);
+    }
+  
+    return thankYouCards;
+  }
+  
+  // Example usage:
+  const namesArray = ["Charlie", "Samip", "Ali"];
+  const event = "birthday";
+  
+  const messages = writeCards(namesArray, event);
+  console.log(messages);
+   
+  function countDown(count) {
+    while (count >= 0) {
+      console.log(count);
+      count--;
+    }
+  }
+  
+  // Example usage:
+  countDown(10);
+  
